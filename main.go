ac58142712e328769f49d3a1b8b5e19e85799d97
Pinku <prantoran@gmail.com>
2023-11-15 17:20:18 -0800

    manual routing using url paths

--- main.go
+++ main.go
@@ -6,9 +6,16 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Welcome to my awesome diary!</h1>")
+	w.Header().Set("Content-Type", "text/html")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Welcome to my awesome diary!</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:prantoran@gmail.com\">prantoran@gmail.com</a>.")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1>We could not find the page you were looking for :(</h1><p>Please email to us if you keep being sent to an invalid page.</p>")
+	}
 }
-
 
 func main() {
 	fmt.Println("Hello, world!")
