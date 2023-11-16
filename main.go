4f9799054e960ea2824a117866dde8eb0e1cdc67
Pinku <prantoran@gmail.com>
2023-11-15 18:12:22 -0800

    httprouter request multiplexer

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
@@ -17,8 +19,13 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "hello, %s\n", ps.ByName("name"))
+}
+
 func main() {
-	mux := &http.ServeMux{}
-	mux.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", mux)
+	router := httprouter.New()
+	// router.GET("/", Index)
+	router.GET("/hello/:name", Hello)
+	http.ListenAndServe(":3000", router)
 }
