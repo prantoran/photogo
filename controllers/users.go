37da6bcfc33834f34c245d640cae1511e57d13a9
Pinku <prantoran@gmail.com>
2023-11-20 23:29:07 -0800

    simplify form parsing

--- controllers/users.go
+++ controllers/users.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/gorilla/schema"
 	"github.com/prantoran/photogo/views"
 )
 
@@ -36,19 +35,10 @@ type SignupForm struct {
 //
 // POST /signup
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
-	if err := r.ParseForm(); err != nil {
-		panic(err)
-	}
-
-	dec := schema.NewDecoder()
 	var form SignupForm
-	if err := dec.Decode(&form, r.PostForm); err != nil {
+	if err := parseForm(r, &form); err != nil {
 		panic(err)
 	}
 
 	fmt.Fprintln(w, form)
-	fmt.Fprintln(w, r.PostForm["email"])
-	fmt.Fprintln(w, r.PostFormValue("email"))
-	fmt.Fprintln(w, r.PostForm["password"])
-	fmt.Fprintln(w, r.PostFormValue("password"))
 }
