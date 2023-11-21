02afff0ccba059d495aee067ddfddbf714677061
Pinku <prantoran@gmail.com>
2023-11-21 00:26:52 -0800

    simplify adding view templates by adding path and extension in views.go

--- views/view.go
+++ views/view.go
@@ -1,6 +1,7 @@
 package views
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 	"path/filepath"
@@ -8,10 +9,17 @@ import (
 
 var (
 	LayoutDir   string = "views/layouts/"
+	TemplateDir string = "views/"
 	TemplateExt string = ".gohtml"
 )
 
 func NewView(layout string, files ...string) *View {
+	fmt.Println("Before:", files)
+	addTemplatePath(files)
+	fmt.Println("After path:", files)
+	addTemplateExt(files)
+	fmt.Println("After ext:", files)
+
 	files = append(files, layoutFiles()...)
 
 	t, err := template.ParseFiles(files...)
@@ -48,4 +56,20 @@ func (v *View) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func (v *View) Render(w http.ResponseWriter, data interface{}) error {
 	w.Header().Set("Content-Type", "text/html")
 	return v.Template.ExecuteTemplate(w, v.Layout, data)
+}
+
+// The input {"home"} would result in the output
+// {"views/home"} if TemplateDir == "views/"
+func addTemplatePath(files []string) {
+	for i, f := range files {
+		files[i] = TemplateDir + f
+	}
+}
+
+// The input {"home"} would result in the output
+// {"home.gohtml"} if TemplateExt == ".gohtml"
+func addTemplateExt(files []string) {
+	for i, f := range files {
+		files[i] = f + TemplateExt
+	}
 }
